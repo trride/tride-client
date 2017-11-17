0f172814a48c1cd06b962993bbf47f9842326de8
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-11-17 18:38:47 +0700

    commented out reduxed

--- src/screens/Reduxed.js
+++ src/screens/Reduxed.js
@@ -1,4 +1,5 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
+export default () => null;
 // import { connect } from "react-redux";
 // import {
 //   Text,
